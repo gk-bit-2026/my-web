5e36d326e4066abf63b998e757a81720865ed43a
gk-bit-2026 <graphikardia@gmail.com>
2026-01-20 23:31:38 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx"; // Simplified path
+import App from "./app/App.tsx";
 import "./styles/index.css";
 
 const container = document.getElementById("root");
