15a21e51c082620ddfe136477ee9afc051ab2f6c
gk-bit-2026 <graphikardia@gmail.com>
2026-01-23 02:13:48 +0530

    Create layout.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,13 @@
+import { TerminalProvider } from '@/context/TerminalContext';
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>
+        <TerminalProvider>
+          {children}
+        </TerminalProvider>
+      </body>
+    </html>
+  );
+}
