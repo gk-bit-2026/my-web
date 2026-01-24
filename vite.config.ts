1461519023ca8838f768855a8a3aacfbf3cefd14
gk-bit-2026 <graphikardia@gmail.com>
2026-01-24 22:39:37 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,10 @@ import path from 'path';
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
+   alias: {
+  '@': path.resolve(__dirname, './src/app'),
+  '@/lib': path.resolve(__dirname, './src/lib'),
+},
   },
   server: {
     host: true, // Exposes to network for mobile testing
